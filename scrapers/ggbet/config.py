e36bcc35a2d764e949a2f45cef64c0b08852755f
maxlamberti <maximilien.lamberti@gmail.com>
2019-08-28 13:27:55 -0700

    add log config files for odds scrapers

---
+++ scrapers/ggbet/config.py
@@ -0,0 +1,31 @@
+
+
+GGBET_URL = 'https://gg.bet/en/counter-strike'
+
+
+LOGGING = {
+	'disable_existing_loggers': False,
+	'version': 1,
+	'formatters': {
+		'simple': {
+			'format': '%(asctime)s - %(levelname)s - %(message)s'
+		},
+	},
+	'handlers': {
+		'console': {
+			'level': 'DEBUG',
+			'formatter': 'simple',
+			'class': 'logging.StreamHandler',
+		}
+	},
+	'loggers': {
+		'PRODUCTION': {
+			'handlers': ['console'],
+			'level': 'INFO',
+		},
+		'DEV': {
+			'handlers': ['console'],
+			'level': 'DEBUG',
+		}
+	}
+}
